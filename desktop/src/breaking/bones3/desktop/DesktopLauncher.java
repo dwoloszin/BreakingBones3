111c2c1bc4e5bf81487689311013d914e98076e4
Dario Woloszin <3h2so4@gmail.com>
2016-05-11 14:38:19 -0300

    first commit

---
+++ desktop/src/breaking/bones3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package breaking.bones3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import breaking.bones3.PlayGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlayGame(), config);
+	}
+}
